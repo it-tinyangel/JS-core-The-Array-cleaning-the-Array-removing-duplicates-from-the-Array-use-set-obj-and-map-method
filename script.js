bb6dc0cb1ad6f27c1608f56e81e0756f71d3d237
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-10-28 18:29:39 +0300

    Array cleaning (using 'Set' object and 'map()' method)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Function to remove duplicates from an array.
+// Function to remove duplicate elements from an array.
 function removeDuplicates(arr) {
 	// return [...new Set(arr.map(item => item.toLowerCase()))];
 	const filteredItemsSet = new Set(arr.map(item => item.toLowerCase()));
